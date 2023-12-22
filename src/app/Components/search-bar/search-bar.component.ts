b7939986ffa5519862cd7a96a95219794d1f7284
Yehia <Yehia>
2023-12-23 01:33:07 +0200

    ADD header

--- src/app/Components/search-bar/search-bar.component.ts
+++ src/app/Components/search-bar/search-bar.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
-
+  isLight: boolean = true;
 }
