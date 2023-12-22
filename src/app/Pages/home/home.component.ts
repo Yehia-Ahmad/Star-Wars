fa9a38ccf10b52dfbe4109b5380e8f4272cdff44
Yehia <Yehia>
2023-12-23 01:33:35 +0200

    UPDATE Home Page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { APIResponse } from './../../Models';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isMale: boolean = true;
+  results: Array<any>;
+  constructor(private api: APIService) {}
 
+  ngOnInit(): void {
+    this.getCharacterList();
+  }
+
+  getCharacterList() {
+    return this.api.getCharacterList().subscribe((res: any) => {
+      this.results = res.results;
+      console.log(this.results);
+    });
+  }
 }
