b8faa313dab1208370aaa0456ef3fd5994a11d26
Yehia <Yehia>
2023-12-27 18:44:24 +0200

    ADD Angular Material Theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { CdkDrag, CdkDropList, CdkDropListGroup } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +12,6 @@ import { NotFoundComponent } from './Pages/not-found/not-found.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { SearchBarComponent } from './Components/search-bar/search-bar.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CdkDrag, CdkDropList, CdkDropListGroup } from '@angular/cdk/drag-drop';
 import { PagenationComponent } from './Components/pagenation/pagenation.component';
 import { CardListComponent } from './Components/card-list/card-list.component';
 import { ResultsComponent } from './Pages/results/results.component';
@@ -32,6 +35,9 @@ import { LoadingInterceptor } from './Interceptors/loading.interceptor';
     CdkDropListGroup,
     CdkDropList,
     CdkDrag,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
